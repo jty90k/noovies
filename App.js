2f5994e674020b43739f864ab83587c3275ebcd8
UniverseStark <jty90k@gmail.com>
2021-03-15 18:14:30 +0900

    2.2 cacheFonts

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
 import { Text, Image } from "react-native";
 import { Asset } from "expo-asset";
+import { Ionicons } from "@expo/vector-icons";
 
 const cacheImages = (images) =>
   images.map((image) => {
@@ -12,14 +14,22 @@ const cacheImages = (images) =>
     }
   });
 
+const cacheFonts = (fonts) =>
+  fonts.map((font) => [
+    Font.loadAsync(font),
+    Font.loadAsync(font),
+    Font.loadAsync(),
+  ]);
+
 export default function App() {
   const [isReady, setIsReady] = useState(false);
-  const loadAssets = async () => {
+  const loadAssets = () => {
     const images = cacheImages([
       "https://www.google.com/search?q=free+high+quality+image&tbm=isch&ved=2ahUKEwiD4O6A7bHvAhXJIaYKHUo7DbUQ2-cCegQIABAA&oq=free+high&gs_lcp=CgNpbWcQARgAMgIIADIECAAQHjIECAAQHjIECAAQHjIECAAQHjIECAAQHjIECAAQHjIECAAQHjIECAAQHjIECAAQHjoECAAQEzoECCMQJzoHCCMQ6gIQJzoFCAAQsQM6CAgAELEDEIMBOggIABAIEB4QEzoGCAAQBRAeOgYIABAIEB5QvpkBWMbbAWCh_gFoCXAAeAKAAYwBiAGqEpIBBDcuMTWYAQCgAQGqAQtnd3Mtd2l6LWltZ7ABCsABAQ&sclient=img&ei=9hZPYIONAsnDmAXK9rSoCw&bih=1196&biw=1764#imgrc=DTsA45p_l9gBfM",
       require("./assets/splash.png"),
     ]);
-    console.log(images);
+    const fonts = cacheFonts([Ionicons.font]);
+    return Promise.all([...images, ...fonts]);
   };
   const onFisnish = () => setIsReady(true);
   return isReady ? (
