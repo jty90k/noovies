26e475c6626562873edf2853e4125246108f9e70
UniverseStark <jty90k@gmail.com>
2021-03-16 17:17:06 +0900

    2.8 Adding the Icons

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,10 +1,12 @@
-import React, { useEffect, useLayoutEffect } from "react";
+import React, { useLayoutEffect } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
 import Search from "../screens/Search";
 import Favs from "../screens/Favs";
 import { getFocusedRouteNameFromRoute } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
+import { Platform } from "react-native";
 
 const Tabs = createBottomTabNavigator();
 
@@ -12,21 +14,46 @@ const getHeaderName = (route) =>
   getFocusedRouteNameFromRoute(route) || "Movies";
 
 // Stack Navigator 안에는 Tab Navigator가 들어가 있다. 하단에 메뉴 클릭 시 각 화면으로 이동 시켜주는 것
-// ({route}) and console.log(route)하게 되면 각 하단 메뉴에 index값을 알 수 있다. 위에서 아래로 (0,1,2,3)이다.
+// console.log(route)를 찍으면 index값을 알 수 있다. 위에서 아래로 (0,1,2,3)이다.
 export default ({ navigation, route }) => {
   //
   useLayoutEffect(() => {
     const name = getHeaderName(route);
     navigation.setOptions({
       title: name,
-      // headerStyle: {
-      //   backgroundColor: name === "TV" ? "blue" : "white",
-      // },
-      //headerShown: name !== "TV",
     });
   }, [route]);
   return (
-    <Tabs.Navigator>
+    <Tabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused }) => {
+          let iconName = Platform.OS === "ios" ? "ios-" : "md-";
+          if (route.name === "Movies") {
+            iconName += "film";
+          } else if (route.name === "TV") {
+            iconName += "tv";
+          } else if (route.name === "Search") {
+            iconName += "search";
+          } else if (route.name === "Favorite") {
+            iconName += "heart";
+          }
+          return (
+            <Ionicons
+              name={iconName}
+              color={focused ? "white" : "grey"}
+              size={26}
+            />
+          );
+        },
+      })}
+      tabBarOptions={{
+        showLabel: false,
+        style: {
+          backgroundColor: "black",
+          borderTopColor: "black",
+        },
+      }}
+    >
       <Tabs.Screen name="Movies" component={Movies} />
       <Tabs.Screen name="TV" component={Tv} />
       <Tabs.Screen name="Search" component={Search} />
