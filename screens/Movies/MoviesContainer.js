4d8733592429a26fe632609209bdd02f2920c795
UniverseStark <jty90k@gmail.com>
2021-03-17 17:10:52 +0900

    4.0 Home Slider

--- screens/Movies/MoviesContainer.js
+++ screens/Movies/MoviesContainer.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { View, Text } from "react-native";
-import { movieApi } from "../api";
+import { movieApi } from "../../api";
+import MoviesPresent from "./MoviesPresenter";
 
 // 다른 곳으로 이동시 navigation을 사용한다.
 export default () => {
   // state를 크게 2가지로 사용할 수 있다. (1. 전체를 받아서 사용, 2. 부분으로 사용)
   const [movies, setMovies] = useState({
+    loading: true,
     nowPlaying: [],
     popular: [],
     upcoming: [],
@@ -18,6 +20,7 @@ export default () => {
     const [popular, popularError] = await movieApi.popular();
     const [upcoming, upcomingError] = await movieApi.upcoming();
     setMovies({
+      loading: false,
       nowPlaying,
       popular,
       upcoming,
@@ -30,9 +33,5 @@ export default () => {
     getData();
   }, []);
 
-  return (
-    <View style={{ flex: 1, backgroundColor: "black" }}>
-      <Text style={{ color: "white" }}>{movies.nowPlaying?.length}</Text>
-    </View>
-  );
+  return <MoviesPresent />;
 };
