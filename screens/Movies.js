dccd1010df9148cb1274fa0fc63ad265ad15051a
UniverseStark <jty90k@gmail.com>
2021-03-17 15:28:31 +0900

    3.2 Divide and Conquer

--- screens/Movies.js
+++ screens/Movies.js
@@ -1,10 +1,20 @@
-import React from "react";
-import { View, Text, Button } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text } from "react-native";
+import { movieApi } from "../api";
 
 // 다른 곳으로 이동시 navigation을 사용한다.
-export default ({ navigation }) => (
-  <View style={{ flex: 1, backgroundColor: "black" }}>
-    <Text>Movies</Text>
-    <Button title="Movie" onPress={() => navigation.navigate("Detail")} />
-  </View>
-);
+export default () => {
+  // state를 크게 2가지로 사용할 수 있다. (1. 전체를 받아서 사용, 2. 부분으로 사용)
+  const getData = async () => {
+    const [nowPlaying, error] = await movieApi.nowPlaying();
+    const [popular, popularError] = await movieApi.popular();
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <View style={{ flex: 1, backgroundColor: "black" }}>
+      <Text>Movies</Text>
+    </View>
+  );
+};
