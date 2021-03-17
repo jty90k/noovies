f363a694575223fdae46c33df8a98b0f09ce6e8c
UniverseStark <jty90k@gmail.com>
2021-03-17 11:14:01 +0900

    3.1 API Layer paty two

--- api.js
+++ api.js
@@ -5,27 +5,28 @@ const TMDB_KEY = "d3a1306c4f4b98101b6f29ff3bd068f3";
 const makeRequest = (path, params) =>
   axios.get(`https://api.themoviedb.org/3${path}`, {
     params: {
-      // ... 뜻은 object의 contents를 get의미이다. 그래서 아래와 같이 page2를 얻는다.
-      //makeRequest("/movies", {page:2})
       ...params,
       apik_key: TMDB_KEY,
     },
   });
 
 export const movieApi = {
-  nowPlaying: () => makeRequest(),
-  popular: () => makeRequest(),
-  upcoming: () => makeRequest(),
-  search: (word) => makeRequest(),
-  movie: (id) => makeRequest(),
-  discover: () => makeRequest(),
+  nowPlaying: () => makeRequest("/movie/now_playing"),
+  popular: () => makeRequest("/movie/popular"),
+  upcoming: () => makeRequest("/movie/upcoming", { region: "kr" }),
+  search: (query) => makeRequest("/search/movie", { query }),
+  movie: (id) => makeRequest(`/movie/${id}`),
+  discover: () => makeRequest("/discover/movie"),
 };
 
 export const tvApi = {
-  today: () => makeRequest(),
-  thisWeek: () => makeRequest(),
-  topRated: () => makeRequest(),
-  popular: () => makeRequest(),
-  search: (word) => makeRequest(),
-  show: (id) => makeRequest(),
+  today: () => makeRequest("/tv/airing_today"),
+  thisWeek: () => makeRequest("/tv/on_the_air"),
+  topRated: () => makeRequest("/tv/top_rated"),
+  popular: () => makeRequest("/tv/popular"),
+  search: (query) => makeRequest("/search/tv", { query }),
+  show: (id) => makeRequest(`/tv/${id}`),
 };
+
+// ... 뜻은 object의 contents를 get의미이다. 그래서 아래와 같이 page2를 얻는다.
+//makeRequest("/movies", {page:2})
--- navigation/Stack.js
+++ navigation/Stack.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import Home from "../screens/Movies";
 import Detail from "../screens/Detail";
 import Tabs from "./Tabs";
 
