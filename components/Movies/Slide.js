8451a4489696fd16e4493ade385d7805ec3ea8ec
UniverseStark <jty90k@gmail.com>
2021-03-18 09:32:07 +0900

    4.3 Slider Content

--- components/Movies/Slide.js
+++ components/Movies/Slide.js
@@ -1,29 +1,55 @@
 import React from "react";
 import styled from "styled-components/native";
 import PropTypes from "prop-types";
-import { Dimensions, Image } from "react-native";
 import { apiImage } from "../../api";
 
-const { width: WIDTH, height: HEIGHT } = Dimensions.get("screen");
-
 const Container = styled.View`
-  width: ${WIDTH}px;
-  height: ${HEIGHT / 4}px;
-  background-color: red;
+  height: 100%;
+  width: 100%;
 `;
 
 const BG = styled.Image`
-    height: 100%,
-    width: 100%;
+  height: 100%;
+  width: 100%;
+  opacity: 0.6;
+  position: absolute;
+`;
+
+const Content = styled.View`
+  flex-direction: row;
+`;
+
+const Data = styled.View`
+  width: 70%;
+`;
+
+const Title = styled.Text`
+  color: white;
+  font-weight: bold;
+  font-size: 18px;
+`;
+
+const Votes = styled.Text`
+  color: white;
+  opacity: 0.7;
+`;
+
+const Overview = styled.Text`
+  color: white;
+  opacity: 0.7;
 `;
 
 //최종적으로 슬라이드 정보 가져와서 화면에 띄운다.
 const Slide = ({ id, title, backgroundImage, votes, overview }) => (
   <Container>
-    <Image
-      style={{ width: "100%", height: "100%" }}
-      source={{ uri: apiImage(backgroundImage) }}
-    />
+    <BG source={{ uri: apiImage(backgroundImage) }} />
+    <Content>
+      <Data>
+        <Title>{title}</Title>
+        <Votes>{votes} / 10</Votes>
+        <Overview>{overview}</Overview>
+      </Data>
+    </Content>
   </Container>
 );
 
