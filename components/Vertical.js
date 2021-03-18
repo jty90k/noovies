4d9e82d3e8f773c1f4893212c18ad69cb46fb1c6
UniverseStark <jty90k@gmail.com>
2021-03-18 21:53:59 +0900

    4.7 Vertical Moive Component part Two

--- components/Vertical.js
+++ components/Vertical.js
@@ -4,19 +4,29 @@ import PropTypes from "prop-types";
 import Poster from "./Poster";
 import Votes from "./Votes";
 import { apiImage } from "../api";
+import { TouchableOpacity } from "react-native";
 
-const Container = styled.View``;
+// 앱화면 중간에 있는 횡스크롤
+
+const Container = styled.View`
+  align-items: center;
+  margin-right: 20px;
+`;
 
 const Title = styled.Text`
   color: white;
+  font-weight: 500;
+  margin: 10px; 0px 5px 0px;
 `;
 
 const Vertical = ({ poster, title, votes }) => (
-  <Container>
-    <Poster url={apiImage(poster)} />
-    <Title>{title}</Title>
-    <Votes votes={votes} />
-  </Container>
+  <TouchableOpacity>
+    <Container>
+      <Poster url={apiImage(poster)} />
+      <Title>{title.length > 10 ? `${title.slice(0, 15)}...` : title}</Title>
+      <Votes votes={votes} />
+    </Container>
+  </TouchableOpacity>
 );
 
 Vertical.propTypes = {
