37b45f15c782023e840c8d77fe8c1abdcf0635f8
UniverseStark <jty90k@gmail.com>
2021-03-26 20:43:38 +0900

    8.1 Detecting Drag

--- screens/Favs/FavsPresenter.js
+++ screens/Favs/FavsPresenter.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Dimensions } from "react-native";
+import { Dimensions, PanResponder } from "react-native";
 import styled from "styled-components/native";
 import { apiImage } from "../../api";
 
@@ -27,10 +27,17 @@ border-radius: 20px
 `;
 
 export default ({ results }) => {
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: (evt, { dx }) => {
+      console.log(dx);
+    },
+  });
+  console.log(panResponder);
   return (
     <Container>
       {results.reverse().map((result) => (
-        <Card key={result.id}>
+        <Card key={result.id} {...panResponder.panHandlers}>
           <Poster source={{ uri: apiImage(result.poster_path) }} />
         </Card>
       ))}
