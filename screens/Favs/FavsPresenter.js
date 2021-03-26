37a9c14b88564150bbb5c646cda889f12dd90811
UniverseStark <jty90k@gmail.com>
2021-03-26 21:49:21 +0900

    8.3 Releasing TopCard

--- screens/Favs/FavsPresenter.js
+++ screens/Favs/FavsPresenter.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Dimensions, PanResponder, Animated } from "react-native";
 import styled from "styled-components/native";
 import { apiImage } from "../../api";
@@ -27,27 +27,55 @@ const styles = {
 };
 
 export default ({ results }) => {
+  const [topIndex, setTopIndex] = useState(0);
   const position = new Animated.ValueXY();
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
     onPanResponderMove: (evt, { dx, dy }) => {
       position.setValue({ x: dx, y: dy });
     },
+    onPanResponderRelease: () => {
+      Animated.spring(position, {
+        toValue: {
+          x: 0,
+          y: 0,
+        },
+      }).start();
+    },
   });
+  // 가장 앞면의 포스터
   return (
     <Container>
-      {results.reverse().map((result) => (
-        <Animated.View
-          style={{
-            ...styles,
-            transform: [...position.getTranslateTransform()],
-          }}
-          key={result.id}
-          {...panResponder.panHandlers}
-        >
-          <Poster source={{ uri: apiImage(result.poster_path) }} />
-        </Animated.View>
-      ))}
+      {results.reverse().map((result, index) => {
+        // 포스터가 상단으로 올라가면 값이 0 가면서 zIndex:1이 된다.
+        if (index === topIndex) {
+          return (
+            <Animated.View
+              style={{
+                ...styles,
+                zIndex: 1,
+                transform: [...position.getTranslateTransform()],
+              }}
+              key={result.id}
+              {...panResponder.panHandlers}
+            >
+              <Poster source={{ uri: apiImage(result.poster_path) }} />
+            </Animated.View>
+          );
+        }
+        // 움직 않는 다음 포스터
+        return (
+          <Animated.View
+            style={{
+              ...styles,
+            }}
+            key={result.id}
+            {...panResponder.panHandlers}
+          >
+            <Poster source={{ uri: apiImage(result.poster_path) }} />
+          </Animated.View>
+        );
+      })}
     </Container>
   );
 };
